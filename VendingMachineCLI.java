f3053aa125594e60f1d68c2db2df8d4c7e70e730
ochoachristian <72667987+ochoachristian@users.noreply.github.com>
2022-07-04 16:44:32 -0400

    Update VendingMachineCLI.java

--- VendingMachineCLI.java
+++ VendingMachineCLI.java
@@ -29,8 +29,7 @@ public class VendingMachineCLI {
 		System.out.println("     Welcome To Vendo-Matic 800     ");
 		System.out.println("************************************");
 		Scanner sc = new Scanner(System.in);
-		VendingMachine v  = new VendingMachine(); // <-- THIS was initially inside the While(True), new instance was created everytime
-		// in a new instance of vendingMachine, amount is initialized to 0, currentStock is set to 5;
+		VendingMachine v  = new VendingMachine();
 		v.createVendingMachine(file);
 
 		while (true) {
@@ -39,12 +38,11 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 
-			     v.displayVendingMachine(); // <-- displays vending machine items
+			     v.displayVendingMachine();
 
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
-				while (true) { // added this line to keep user in PURCHASE_OPTIONS array
-					// After the product is dispensed, the machine must update its balance accordingly AND return the customer to the Purchase menu.
+				while (true) {
 					String nextChoice = (String) menu.getChoiceFromOptions(PURCHASE_OPTIONS);
 
 					if (nextChoice.equals(FEED_MONEY)) {
@@ -53,7 +51,7 @@ public class VendingMachineCLI {
 								System.out.println("Please Enter Amount");
 								double amountEntered = Double.parseDouble(sc.nextLine());
 								if (amountEntered == 1 || amountEntered == 2 || amountEntered == 5 || amountEntered == 10) {
-									v.feedMoney(amountEntered, logFile); // <-- Enters money into vendingMachine, takes in double parameter
+									v.feedMoney(amountEntered, logFile); 
 								} else {
 									System.out.println("Not A Valid Input!");
 								}
@@ -67,18 +65,17 @@ public class VendingMachineCLI {
 
 						System.out.println("Enter Code");
 						String codeEntered = sc.nextLine();
-						v.selectProduct(codeEntered, logFile);   // <-- Selecting product, takes in String parameter
-
+						v.selectProduct(codeEntered, logFile);  
 					} else if (nextChoice.equals(FINISH_TRANSACTION)) {
-						 v.giveChange(logFile); // <-- prints change in coins, updates amount to 0
-                         break; // <-- ends true loop
+						 v.giveChange(logFile); 
+                         break; 
 					}
 				}
 			}else if (choice.equals(MAIN_MENU_EXIT)){
 				System.out.println(" ***************************************************");
 				System.out.println("     Thank you For Shopping At Vendo-Matic 800     ");
 				System.out.println("***************************************************");
-				  break; //<-- ends loop, same as above
+				  break;
 			}
 		}
 	}
